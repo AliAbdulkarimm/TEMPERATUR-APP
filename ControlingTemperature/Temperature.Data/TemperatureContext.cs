9d16850c96bc3c2448df7ed60fb004aaed0a7759
AliAbdulkarimm <71064901+AliAbdulkarimm@users.noreply.github.com>
2021-01-29 10:45:19 +0100

    Add files via upload

---
+++ ControlingTemperature/Temperature.Data/TemperatureContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+
+namespace Temperature.Data
+{
+    public class TemperatureContext : DbContext
+    {
+        public DbSet<TemperatureList> TemperatureData { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            NewMethod(optionsBuilder);
+        }
+
+        private static void NewMethod(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBTemperature;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
---
+++ ControlingTemperature/Temperature.Data/TemperatureList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Temperature.Data
+{
+    public record TemperatureList
+    {
+
+        public int Id { get; set; }
+        public DateTime Datetime { get; set; }
+        public string Location { get; set; }
+        public double Temperature { get; set; }
+        public double Humidity { get; set; }
+
+        public static TemperatureList ParseFromCSV(string line)
+        {
+            var columns = line.Split(',');
+            if (columns[0] == "Datetime")
+                return null;
+            return new TemperatureList
+            {
+                Datetime = DateTime.Parse(columns[0]),
+                Location = columns[1],
+                Temperature = double.Parse(columns[2], CultureInfo.InvariantCulture),
+                Humidity = double.Parse(columns[3])
+
+            };
+        }
+    }
+}
